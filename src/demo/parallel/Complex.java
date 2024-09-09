f8b38d0de3f45571d26602574970b863ca6cc996
Tonitrus <gromahacer@gmail.com>
2024-09-09 22:32:04 +0300

    add other operation

--- src/demo/parallel/Complex.java
+++ src/demo/parallel/Complex.java
@@ -85,6 +85,18 @@ public class Complex {
         return this;
     }
 
+    public Complex minus(Complex b) {
+        re -= b.re;
+        im -= b.im;
+        return this;
+    }
+
+    public Complex division(Complex b) {
+        Complex a = this;
+        re = (a.re * b.re + a.im + b.im) / (b.re * b.re + b.im * b.im);
+        im = (b.re * a.im - a.re * b.im) / (b.re * b.re + b.im * b.im);
+        return this;
+    }
     /**
      * Square of Complex object's length, we're using square of length to 
      * eliminate the computation of square root
